57d1ded414a2304a0bce5ff6d0d951335515e152
davisonjoshua <167842943+davisonjoshua@users.noreply.github.com>
2024-09-12 14:24:54 +0530

    Add files via upload

---
+++ pythonfiles/python2.py
@@ -0,0 +1,12 @@
+# Input the current traffic light color
+light = input("Enter the traffic light color (Red, Yellow, Green): ").strip().lower()
+
+# Determine the action based on the traffic light color
+if light == "red":
+    print("Stop")
+elif light == "yellow":
+    print("Slow down")
+elif light == "green":
+    print("Go")
+else:
+    print("Invalid color")
